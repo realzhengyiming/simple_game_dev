d9c65c2992513db11dfcf837ef9b4c1202f2e7df
yiming <realzhengyiming@outlook.com>
2024-12-15 22:22:38 +0800

    增加了基础的shader 和基础的移动

--- characters/player_move.gd
+++ characters/player_move.gd
@@ -11,24 +11,29 @@ var target_position
 # 新增变量，用于记录当前正在移动的方向，初始化为空字符串，表示尚未开始移动或者移动已结束
 var current_moving_direction = ""  
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 func _physics_process(delta):
 	# 在每帧物理处理过程中执行以下逻辑
 	if is_moving:
 		# 如果角色当前正在移动，创建一个补间动画（Tween）对象来处理移动动画效果
-		var tween = get_tree().create_tween()
+		var tween = get_tree().create_tween()#.set_parallel(true)
 		# 设置补间动画的过渡类型为正弦过渡（TRANS_SINE），使移动动画更加平滑自然
 		tween.set_trans(Tween.TRANS_SINE)
 		# 设置补间动画的缓动效果为淡入淡出（EASE_IN_OUT），让移动开始和结束时的速度变化更柔和
 		tween.set_ease(Tween.EASE_IN_OUT)
 		# 通过补间动画将角色的当前位置（"position"属性）逐渐改变到目标位置（target_position），动画时长设置为0.2秒
-		tween.tween_property(self, "position", target_position, 0.2)
+		tween.tween_property(self, "position", target_position, 0.5)
+		#tween.tween_property(self, "position", target_position, 0.5)
+
 		# 移动动画完成后，将角色的移动状态标记为false，表示角色已停止移动
 		is_moving = false
 		# 清空当前移动方向记录，以便下次能正确响应新的方向键输入
 		current_moving_direction = ""  
+	
+		animation_player.play("walk")
 	else:
 		# 角色当前处于静止状态，开始处理方向键输入逻辑
-
 		# 检测向右方向键是否刚刚被按下，并且当前没有正在进行的移动操作（即current_moving_direction为空字符串）
 		if Input.is_action_just_pressed("right") && current_moving_direction == "":
 			# 如果满足条件，计算角色向右移动后的目标位置，基于当前位置在x轴方向增加grid_pix个像素
